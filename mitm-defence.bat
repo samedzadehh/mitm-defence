f881c6f32a343c3bbec2e32348d4689ff68282c0
Huseyn Samedzade <129556088+samedzadehh@users.noreply.github.com>
2024-08-26 02:09:10 +0400

    mitm-defence.bat
    Bat file

--- mitm-defence.bat
+++ mitm-defence.bat
@@ -1,14 +1,14 @@
 @echo off
 python --version >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Python yuklu degil.
-    echo Python 3.12.5 indiriliyor ve yukleniyor...
+    echo Python is not installed.
+    echo Python 3.12.5 is being downloaded and installed...
     curl -o python-installer.exe https://www.python.org/ftp/python/3.12.5/python-3.12.5-amd64.exe
     python-installer.exe /quiet InstallAllUsers=1 PrependPath=1
-    echo Python 3.12.5 yuklendi.
+    echo Python 3.12.5 has been installed
 
 ) else (
-    echo Python yuklu.
+    echo Python is installed.
     python --version
 
 )
